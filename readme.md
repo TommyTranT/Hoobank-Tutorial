3536bc27eaf575e4edeaf345f83a581e95fc082a
Tommy Tran <haitran1995@gmail.com>
2022-12-01 23:01:40 -0500

    add mobile menu button

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 - Learning Outcomes
   - Structuring components in an organized way and importing them to app through index.js
   - using tailwind classnames to stylize components
+  - using if statement for styling.
+    if index of array equals the last array, margin right 0, else margin right 10
+    className={`${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,30 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div className="w-full flex py-6 justify-between items-center navbar">
+    <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
-    </div>
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Version: If toggle state is true, show img close otherwise show img menu */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img src={toggle ? close : menu} />
+      </div>
+    </nav>
   );
 };
 
