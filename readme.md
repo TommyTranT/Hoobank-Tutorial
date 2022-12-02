65ff68269383e0be8d9be5551f374a754af80e80
Tommy Tran <haitran1995@gmail.com>
2022-12-01 22:29:53 -0500

    add logo

---
+++ readme.md
@@ -0,0 +1,3 @@
+- Learning Outcomes
+  - Structuring components in an organized way and importing them to app through index.js
+  - using tailwind classnames to stylize components
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+
+// array of navbar id and title
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+    </div>
+  );
 };
 
 export default Navbar;
