310597ac1a357a1881951d37cb56b15472f954a4
Tommy Tran <haitran1995@gmail.com>
2022-12-02 04:13:42 -0500

    add paragraph for hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,6 @@ const Hero = () => (
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
-      {/* Discount Bar */}
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
         <p className={`${styles.paragraph} ml-2`}>
@@ -16,23 +15,25 @@ const Hero = () => (
           <span className="text-white">1 MONTH</span> ACCOUNT
         </p>
       </div>
-      {/* Discount Bar */}
 
-      {/* Text */}
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The Next <br className="sm:block hidden" />{" "}
           <span className="text-gradient">Generation</span> <br />
-          Payment Method.
         </h1>
-        {/* Get Started Circle */}
-        <div className=""></div>
-        {/* Get Started Circle */}
         <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted />
         </div>
       </div>
-      {/* Text */}
+
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most
+        likely to fit your needs. We examine annual percentage rates, annual
+        fees.
+      </p>
     </div>
   </section>
 );
